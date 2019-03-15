f40b3622b0c13bd1597b57fb943a313a45516c32
a.snigirev <dmbsseller@gmail.com>
2019-03-15 18:20:33 +0300

    AlfaCar43
    +custom error page (Web.config)

---
+++ AlfaCar43Project/AlfaCar43Project/Controllers/ErrorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlfaCar43Project.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+
+        public ActionResult Forbidden()
+        {
+            Response.StatusCode = 403;
+            return View();
+        }
+
+        public ActionResult UploadToLarge()
+        {
+            return View();
+        }
+    }
+}
